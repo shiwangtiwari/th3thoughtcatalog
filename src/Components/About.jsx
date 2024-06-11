1daecb3ebd0c30126bb2cb3304a23e615ccdd861
Shiwang Tiwari <shiwangtiwari5@gmail.com>
2024-06-11 14:43:51 +0530

    Added a animation to the readmore button

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,8 +11,8 @@ function About() {
       <div className="w-full flex flex-col md:flex-row border-t-[2px] pt-10 mt-10 md:mt-20 border-[#a1b562]">
         <div className="w-full md:w-1/2 md:pr-5">
           <h1 className="text-[4vw] md:text-[2.5vw]">Our Approach</h1>
-          <button className="flex uppercase gap-3 md:gap-10 items-center px-5 md:px-10 py-4 md:py-6 bg-zinc-900 rounded-full mt-5 md:mt-10 text-white">Read More
-          <div className="w-1 h-1 md:w-2 md:h-2 bg-zinc-100 rounded-full"></div></button>
+          <button className="flex uppercase gap-3 md:gap-10 items-center px-5 md:px-10 py-4 md:py-6 bg-zinc-900 rounded-full mt-5 md:mt-10 text-white read-more-btn">Read More
+          <div className="dot w-1 h-1 md:w-2 md:h-2 bg-zinc-100 rounded-full"></div></button>
         </div>
         <div className="w-full md:w-1/2 h-[30vh] md:h-[60vh] bg-cover bg-picture rounded-3xl mt-5 md:mt-0"></div>
       </div>
